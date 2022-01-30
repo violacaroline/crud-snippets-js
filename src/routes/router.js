8d91972255fb4e49a0143c34a82cf2ae77cf5e04
Caroline Akesson <ca223pw@student.lnu.se>
2022-01-29 18:45:26 -0600

    Succesfully get snippets/index and snippets/create

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,11 +3,14 @@
  */
 import express from 'express'
 import { router as homeRouter } from './home-router.js'
+import { router as snippetRouter } from './snippets-router.js' // TEST ADDED BY ME
 
 export const router = express.Router()
 
 router.use('/', homeRouter)
 
+router.use('/snippets', snippetRouter) // TEST ADDED BY ME, SOMETHING IS UP HERE DOES NOT WORK WITHOUT /SNIPPETS
+// BUT SOMETIME ADDS SNIPPET/SNIPPET WHEN PRESSING LINKS ON HOME PAGE
 router.use('*', (req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
